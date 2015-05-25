78b22eeefe8dc8785db5dc764d0a84b01e862d7a
Hüseyin ZENGİN <huseyinzengin91@gmail.com>
2015-05-25 18:36:48 +0300

    Initial commit

---
+++ Simit.Wrapper.AWS/S3.cs
@@ -0,0 +1,59 @@
+﻿namespace Simit.Wrapper.AWS
+{
+    #region Using Directive
+
+    using Amazon;
+    using Amazon.S3;
+    using Amazon.S3.Transfer;
+    using System;
+    using System.IO;
+
+    #endregion Using Directive
+
+    public class S3
+    {
+        #region Public Methods
+
+        public void PutObject(string bucketName, string key, string fileName, Stream file)
+        {
+            if (bucketName == null) throw new ArgumentNullException("bucketName");
+            if (key == null) throw new ArgumentNullException("key");
+            if (fileName == null) throw new ArgumentNullException("fileName");
+            if (file == null) throw new ArgumentNullException("file");
+
+            string clientAccessKey = System.Configuration.ConfigurationManager.AppSettings["aws:s3:accesskey"];
+            string clientSecretKey = System.Configuration.ConfigurationManager.AppSettings["aws:s3:secretkey"];
+
+            TransferUtility utility = new TransferUtility(clientAccessKey, clientSecretKey, RegionEndpoint.EUWest1);
+
+            utility.Upload(new TransferUtilityUploadRequest
+            {
+                BucketName = bucketName,
+                Key = key + "/" + fileName,
+                InputStream = file,
+                CannedACL = S3CannedACL.PublicRead
+            });
+        }
+
+        public void DownloadObject(string bucketName, string key, string fileName, string saveFilePath)
+        {
+            if (bucketName == null) throw new ArgumentNullException("bucketName");
+            if (key == null) throw new ArgumentNullException("key");
+            if (fileName == null) throw new ArgumentNullException("fileName");
+
+            string clientAccessKey = System.Configuration.ConfigurationManager.AppSettings["aws:s3:accesskey"];
+            string clientSecretKey = System.Configuration.ConfigurationManager.AppSettings["aws:s3:secretkey"];
+
+            TransferUtility utility = new TransferUtility(clientAccessKey, clientSecretKey, RegionEndpoint.EUWest1);
+
+            TransferUtilityDownloadRequest transferUtilityDownloadRequest = new TransferUtilityDownloadRequest();
+            transferUtilityDownloadRequest.BucketName = bucketName;
+            transferUtilityDownloadRequest.Key = key + "/" + fileName;
+            transferUtilityDownloadRequest.FilePath = saveFilePath;
+
+            utility.Download(transferUtilityDownloadRequest);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Simit.Wrapper.AWS/SES.cs
@@ -0,0 +1,89 @@
+﻿namespace Simit.Wrapper.AWS
+{
+    #region Using Directive
+
+    using Amazon;
+    using Amazon.SimpleEmail;
+    using Amazon.SimpleEmail.Model;
+    using System;
+    using System.Collections.Generic;
+
+    #endregion Using Directive
+
+    public class SES
+    {
+        #region Private Fields
+
+        private AmazonSimpleEmailServiceClient serviceClient;
+        private const string EmailAddress = "aws:ses:fromemail";
+        private const string AccessKeyIDName = "aws:ses:accesskey";
+        private const string SecretAccessKey = "aws:ses:secretkey";
+
+        #endregion Private Fields
+
+        #region Public Constructor
+
+        public SES()
+        {
+            string[] requiredKeys = new string[] { EmailAddress, AccessKeyIDName, SecretAccessKey };
+
+            foreach (string key in requiredKeys)
+            {
+                if (System.Configuration.ConfigurationManager.AppSettings[key] == null)
+                {
+                    throw new Exception(key + " not found in appSettings");
+                }
+            }
+        }
+
+        #endregion Public Constructor
+
+        #region Private Properties
+
+        private string SenderEmailAddress
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings[EmailAddress];
+            }
+        }
+
+        private AmazonSimpleEmailServiceClient ServiceClient
+        {
+            get
+            {
+                if (this.serviceClient == null)
+                    this.serviceClient = new AmazonSimpleEmailServiceClient(System.Configuration.ConfigurationManager.AppSettings[AccessKeyIDName], System.Configuration.ConfigurationManager.AppSettings[SecretAccessKey], RegionEndpoint.USEast1);
+
+                return this.serviceClient;
+            }
+        }
+
+        #endregion Private Properties
+
+        public void SendEmail(string to, string subject, string htmlBody, List<string> bcc = null)
+        {
+            this.SendEmail(new List<string> { to }, subject, htmlBody, bcc);
+        }
+
+        public void SendEmail(List<string> to, string subject, string htmlBody, List<string> bcc = null)
+        {
+            Destination destination = new Destination();
+            destination.ToAddresses = to;
+            destination.BccAddresses = bcc;
+
+            Content subjectContent = new Content { Charset = "UTF-8", Data = subject };
+            Content bodyContent = new Content { Charset = "UTF-8", Data = htmlBody };
+
+            SendEmailRequest request = new SendEmailRequest()
+            {
+                Destination = destination,
+                Source = this.SenderEmailAddress,
+                ReturnPath = this.SenderEmailAddress,
+                Message = new Message(subjectContent, new Body() { Html = bodyContent })
+            };
+
+            this.ServiceClient.SendEmail(request);
+        }
+    }
+}
